9bddc3e8a56620dce3049bae7000a8da22648620
TyWalter <walterty89@gmail.com>
2024-03-15 14:50:34 -0500

    Updated indenting

--- assets/script.js
+++ assets/script.js
@@ -3,21 +3,21 @@ const randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
 function listTopics() {
     for(let x = 0; x < topics.length; x++) {
-    console.log(topics[x]);
+        console.log(topics[x]);
     }
 }
 
 function selectTopic() {
     if (randomTopic === 'HTML') {
-     console.log("Let's study HTML!");
+        console.log("Let's study HTML!");
     } else if (randomTopic === 'CSS') {
-     console.log("Let's study CSS!");
+        console.log("Let's study CSS!");
     } else if (randomTopic === 'Git') {
-     console.log("Let's study Git!");
+        console.log("Let's study Git!");
     } else if (randomTopic === 'JavaScript') {
-     console.log("Let's study JavaScript!");
+        console.log("Let's study JavaScript!");
     } else {
-     console.log('Please try again!');
+        console.log('Please try again!');
     }
 }
 console.log("Here are the topics we learned through Prework:");
